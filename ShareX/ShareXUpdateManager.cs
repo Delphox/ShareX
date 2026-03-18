009a084d30fe985a326a07e23a6a86e81e09f914
Delphox <smugleaf@live.com>
2026-03-18 19:07:47 -0300

    point updates to my repo

--- ShareX/ShareXUpdateManager.cs
+++ ShareX/ShareXUpdateManager.cs
@@ -35,16 +35,16 @@ namespace ShareX
         {
             if (UpdateChannel == UpdateChannel.Dev)
             {
-                return new GitHubUpdateChecker("ShareX", "DevBuilds")
+                return new GitHubUpdateChecker("Delphox", "ShareX")
                 {
-                    IsDev = true,
+                    IsDev = false,
                     IsPortable = Program.Portable,
                     IgnoreRevision = true
                 };
             }
             else
             {
-                return new GitHubUpdateChecker("ShareX", "ShareX")
+                return new GitHubUpdateChecker("Delphox", "ShareX")
                 {
                     IsPortable = Program.Portable,
                     IncludePreRelease = UpdateChannel == UpdateChannel.PreRelease,
